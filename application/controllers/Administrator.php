f2506420299f54643865363cb2c23f52ddb68ffe
Brent Macatangay <brent@mcbrent.com>
2018-09-29 17:40:04 +0800

    Fix issue on add admin

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -37,20 +37,19 @@ class Administrator extends Controller {
 		$adminMapper = new App\Mapper\AdminMapper();
 		$basicContactMapper = new App\Mapper\BasicContactMapper();
 
-		$regionMapper = new App\Mapper\RegionMapper();
-		$countryMapper = new App\Mapper\CountryMapper();
 		$data = array(
-				'region_id' => ''
-			,	'region_country_id' => ''
-			,	'region_code' => ''
-			,	'region_desc' => ''
+				'admin_first_name' => ''
+			,	'admin_middle_name' => ''
+			,	'admin_last_name' => ''
+			,	'admin_name_ext' => ''
+			,	'admin_gender' => 'male'
 		);
 		if(!empty($_POST)){
 				$insert_basic_contact = array(
-					'bc_first_name'			=>$_POST['first-name']
-				,	'bc_middle_name'	=>$_POST['middle-name']
-				,	'bc_last_name'	=>$_POST['last-name']
-				,	'bc_name_ext'	=>$_POST['name-ext']
+					'bc_first_name'			=>$_POST['admin-first-name']
+				,	'bc_middle_name'	=>$_POST['admin-middle-name']
+				,	'bc_last_name'	=>$_POST['admin-last-name']
+				,	'bc_name_ext'	=>$_POST['admin-name-ext']
 				,	'bc_phone_num1'	=>''
 				,	'bc_phone_num2'	=>''
 				,	'bc_phone_num3'	=>''
@@ -59,20 +58,24 @@ class Administrator extends Controller {
 				);
 				$bc_id = $basicContactMapper->insert($insert_basic_contact);
 
-				$insert_user = array(
-					'user_name'			=>$_POST['user-id']
+				 $insert_user = array(
+					'user_name'			=>$_POST['user-name']
 				,	'user_password'	=>$_POST['user-password']
 				,	'user_email'	=>$_POST['user-email']
 				,	'user_type'	=> 1
+				,	'user_fm_id'	=> 0
 				);
-				$userMapper->insert($insert_user);
+				$user_id =$userMapper->insert($insert_user);
 
 				$insert_admin = array(
-					'admin_user_name'	=> $insert_user['user_name']
+					'admin_user_id'	=> $user_id
 				,	'admin_bc_id'	=> $bc_id
 				);
 				$adminMapper->insert($insert_admin);
-
+				$this->set_alert(array(
+					'message'=>'<i class="fa fa-check"></i> Successfully added a administrator!'
+				,	'type'=>'success'
+				));
 		}
 		$this->_data['action'] = 'add';
 		$this->_data['form_data'] = $data;
