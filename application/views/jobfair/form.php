7e1398e155f8fe9bdd1b485e9ecf67d0dd43ad3b
Brent Macatangay <brent@mcbrent.com>
2018-11-25 16:04:16 +0800

    Fix conflicts

---
+++ application/views/jobfair/form.php
@@ -0,0 +1,69 @@
+<div class="wrapper wrapper-content animated fadeInLeft">
+  <div class="row">
+    <div class="col-lg-12 text-right">
+      <button class="btn btn-primary has-tooltip form-submit" title="Save" data-form="main-form"><i class="fa fa-file"></i></button>
+    </div>
+    <div class="col-lg-12">
+        <div class="ibox">
+            <div class="ibox-title">
+                <h5><?php echo ucfirst($action) ?> Job Fair</h5>
+                <div class="ibox-tools">
+                    <a class="collapse-link">
+                        <i class="fa fa-chevron-up"></i>
+                    </a>
+                    <a class="fullscreen-link">
+                        <i class="fa fa-expand"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="ibox-content">
+              <form id="main-form" action="" method="POST" class="form-horizontal">
+                  <div class="form-group">
+                    <div class="col-sm-9">
+                      <h5>Job Fair Title</h5>
+                      <input type="text" name="job-fair-title" class="form-control" placeholder="Job Fair Title" value="<?php echo $form_data['js_title']?>">
+                    </div>
+                    <div class="col-sm-3">
+                      <h5>&nbsp;</h5>
+                      <label class="checkbox-inline i-checks"> <input type="checkbox" value="active" name="job-fair-active" <?php echo ($form_data['js_is_current'])? 'checked' : '' ?>> <i></i> Active </label>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-md-6">
+                      <h5>Start Date</h5>
+                      <input type="text" name="job-fair-start-date" class="datepicker form-control" autocomplete="off" placeholder="mm/dd/yyyy" value="<?php echo ($form_data['js_date_from'])? date('m/d/Y', strtotime($form_data['js_date_from'])) : '' ?>"/>
+                    </div>
+                    <div class="col-md-6">
+                      <h5>End Date</h5>
+                      <input type="text" name="job-fair-end-date" class="datepicker form-control" autocomplete="off" placeholder="mm/dd/yyyy" value="<?php echo ($form_data['js_date_to'])? date('m/d/Y', strtotime($form_data['js_date_to'])) : ''  ?>"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-12">
+                      <h5>Status</h5>
+                      <input type="text" name="job-fair-status" class="form-control" maxlength="15" placeholder="Job Fair Status" value="<?php echo $form_data['jf_is_status']?>"/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-12">
+                      <h5>Summary</h5>
+                      <textarea name="job-fair-summary" class="text-editor"><?php echo $form_data['js_summary']?></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-12">
+                      <label class="checkbox-inline i-checks"> <input type="checkbox" value="active" name="job-fair-announcement"> <i></i> Click Here to send email notification on all registered </label>
+                    </div>
+                  </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+  </div>
+
+
+</div>
+
+<script src="<?php echo JS_DIR ?>components/utility/add_jobfair.js"></script>
---
+++ application/views/utility/jobfair/list.php
@@ -0,0 +1,61 @@
+<div class="wrapper wrapper-content animated fadeInLeft">
+  <div class="row">
+      <div class="col-lg-12">
+              <div class="ibox float-e-margins">
+                  <div class="ibox-title">
+                      <h5>Job Fair</h5>
+                      <div class="ibox-tools">
+                          <a class="collapse-link">
+                              <i class="fa fa-chevron-up"></i>
+                          </a>
+                          <a class="close-link">
+                              <i class="fa fa-times"></i>
+                          </a>
+                      </div>
+                  </div>
+                  <div class="ibox-content">
+                    <div class="row">
+                      <div class="col-md-12 text-right">
+                        <a href="<?php echo DOMAIN?>utility/add-job-fair" class="btn btn-info btn-outline"><i class="fa fa-plus"></i> ADD JOB FAIR</a>
+                      </div>
+                    </div>
+                    <div class="table-responsive">
+                      <table class="table table-striped table-bordered table-hover datatable-basic" data-config="{}">
+                        <thead>
+                          <tr>
+                              <th class="col-md-4 text-center">Title</th>
+                              <th class="col-md-2 text-center">From</th>
+                              <th class="col-md-2 text-center">To</th>
+                              <th class="col-md-2 text-center">Status</th>
+                              <th class="col-md-1 text-center">Active</th>
+                              <th class="col-md-1 text-center no-sort">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php foreach($job_fair_list as $job_fair){ ?>
+                          <tr class="<?php echo ($job_fair['js_is_current'])? 'success' : '' ?>">
+                            <td><?php echo $job_fair['js_title'] ?></td>
+                            <td class="text-center"><?php echo date('F j, Y', strtotime($job_fair['js_date_from'])) ?></td>
+                            <td class="text-center"><?php echo date('F j, Y', strtotime($job_fair['js_date_to'])) ?></td>
+                            <td><?php echo $job_fair['jf_is_status'] ?></td>
+                            <td class="text-center"><?php echo ($job_fair['js_is_current'])? 'Active' : '' ?></td>
+                            <td class="text-center">
+                              <div >
+                                <a class="btn btn-info has-tooltip" href="<?php echo DOMAIN.'utility/edit-job-fair/'.$job_fair['jf_id'] ?>" title="Edit"><i class="fa fa-pencil"></i></a>
+                              </div>
+                            </td>
+
+                          </tr>
+                          <?php } ?>
+                        </tbody>
+                      </table>
+                    </div>
+
+                  </div>
+              </div>
+          </div>
+    </div>
+
+</div>
+
+<script src="<?php echo JS_DIR ?>components/utility/jobfair_list.js"></script>
